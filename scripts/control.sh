f730afd0cd4807b800bbe71a00430a278669ab8f
Oliver vBK <olivervbk@gmail.com>
2013-04-04 01:14:20 -0300

    (upd)-bug fixes

--- scripts/control.sh
+++ scripts/control.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. config
+. $(pwd)/$(dirname $0)/config
 if [[ $? -ne 0 ]]; then
 	echo "Config error!"
 	exit 255
@@ -16,29 +16,49 @@ if [[ "$opt" == "check_interface" ]]; then
 fi
 
 if [[ "$opt" == "check_install" ]]; then
-	version=$(reaver -h 2>&1 | grep "Reaver" | cut -d' ' -f2)
+	version=$($REAVER_BIN -h 2>&1 | grep "Reaver" | cut -d' ' -f2)
 	if [[ "$version" != "$SUPPORTED_REAVER_VERSION" ]]; then
 		echo "Unsupported version!"
 		exit 3;
 	fi
 fi
 
+if [[ "$opt" == "configure" ]]; then
+	echo "rmmod $MODULE"
+	rmmod $MODULE
+	echo "insmod $MODULE"
+	insmod $MODULE
+	echo "airmon-ng start $WIRELESS"
+	airmon-ng start $WIRELESS
+fi
+
 if [[ "$opt" == "start_wash" ]]; then
 	#TODO:check pid and remove	
 
-	wash -i $INTERFACE -o $WASH_OUTPUT &>$WASH_LOG &
+	$WASH_BIN -i $INTERFACE -o $WASH_OUTPUT &>$WASH_LOG &
 	wash_pid=$!
 	
 	echo $wash_pid
 	echo $wash_pid > $WASH_PID
 fi
 	
-if [[ "$opt" == "check_wash" ]]; then
+if [[ "$opt" == "get_wash" ]]; then
 	#TODO: format output
-	#TODO: check if running
 	#TODO: check log?
 	
-	tail -n+3 $WASH_OUTPUT
+	tail -n+3 $WASH_OUTPUT | awk '{print $1" "$2" "$3" "$6}'
+fi
+
+if [[ "$opt" == "check_wash" ]]; then
+	wash_pid=$(cat $WASH_PID 2>/dev/null)
+	if [[ "$wash_pid" ]]; then
+		kill -0 $wash_pid
+		if [[ $? -eq 0 ]]; then
+			echo $wash_pid
+		else
+			echo > $WASH_PID
+		fi
+	fi
 fi
 
 if [[ "$opt" == "stop_wash" ]]; then
@@ -64,14 +84,14 @@ if [[ "$opt" == "start_reaver" ]]; then
 	
 	#TODO:check pid and remove	
 	
-	reaver -i $INTERFACE -b $bssid -c  $channel -o $REAVER_OUTPUT $REAVER_OPTS &>$REAVER_LOG &
+	$REAVER_BIN -i $INTERFACE -b $bssid -c  $channel -o $REAVER_OUTPUT $REAVER_OPTS &>$REAVER_LOG &
 	reaver_pid=$?
 	
 	echo $reaver_pid
 	echo $reaver_pid > $REAVER_PID
 fi
 
-if [[ "$opt" == "check_reaver" ]]; then
+if [[ "$opt" == "get_reaver" ]]; then
 	if [[ $# -ne 2 ]]; then
 		echo "$opt <reaver-output-file>"
 		exit 1;
@@ -86,6 +106,18 @@ if [[ "$opt" == "check_reaver" ]]; then
 		# TODO check for errors...
 		echo "should check for errors..."
 	fi
+fi
+
+if [[ "$opt" == "check_reaver" ]]; then
+        reaver_pid=$(cat $REAVER_PID 2>/dev/null)
+        if [[ "$reaver_pid" ]]; then
+                kill -0 $reaver_pid
+                if [[ $? -eq 0 ]]; then
+                        echo $reaver_pid
+                else
+                        echo > $REAVER_PID
+                fi
+        fi
 fi
 
 if [[ "$opt" == "stop_reaver" ]]; then
